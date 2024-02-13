42b2ebd059484bd07e68decc148cea7c0910b1a7
Sakshi Maheshwari <sakshicoder800@gmail.com>
2024-02-13 16:45:39 +0530

    Header footer mainsection

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import MainSection from './MainSection'
 
 const Home = () => {
   return (
-    <div>
-     <h1> Home Page component 1 </h1>
-    </div>
+   <>
+<Header/>
+<MainSection/>
+<Footer/>
+
+   </>
   )
 }
 
---
+++ src/component/MainSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainSection = () => {
+  return (
+    <div>
+      Main
+    </div>
+  )
+}
+
+export default MainSection
