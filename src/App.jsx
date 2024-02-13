6c3bbae337b05715f8e1dfd1fe8d06a526747d83
Sakshi Maheshwari <sakshicoder800@gmail.com>
2024-02-13 16:40:31 +0530

    add ed

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,9 @@ import Home from './component/Home'
 function App() {
 
   return (
-    <>
-<<<<<<< HEAD
-     <Home/>
-     
-=======
->>>>>>> 8d23490028c19870f7efacb45afb2203897de56b
-    </>
+  <>
+  <Home/>
+  </>
   )
 }
 
