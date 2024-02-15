bfef2ff2e5a2723e10dfeea260ba513da2da90cf
Sakshi Maheshwari <sakshicoder800@gmail.com>
2024-02-15 13:56:21 +0530

    error solve

--- src/component/MainSection/MainSection.jsx
+++ src/component/MainSection/MainSection.jsx
@@ -1,42 +1,39 @@
 import React from "react";
-import "./MainSection.css";
 
 const MainSection = () => {
   return (
     <>
-<<<<<<< HEAD:src/component/MainSection.jsx
-      <div className="container" >
-=======
-      <div className="container" style={{ backgroundColor: "#f4f3cd" }}>
->>>>>>> 2031a74508eebe98be7dea53ae471baf8515e713:src/component/MainSection/MainSection.jsx
-        <div className="top-section">
-          <div className="head">
-            <div className="img">
-              <img src="" alt="" />
+      <div className="container">
+        <div className="container" style={{ backgroundColor: "#f4f3cd" }}>
+          <div className="top-section">
+            <div className="head">
+              <div className="img">
+                <img src="" alt="" />
+              </div>
+              <div className="logo"></div>
+              <h3 className=""> Link_Shortner</h3>
+              <p className="small"> * .... free and always will be.</p>
             </div>
-            <div className="logo"></div>
-            <h3 className=""> Link_Shortner</h3>
-            <p className="small"> * .... free and always will be.</p>
           </div>
-        </div>
-        <div className="form-section">
-          <div className="input-field">
-            <input
-              type="text"
-              className="search-inp"
-              placeholder=" https://www.link_shortner.com"
-            />
-            <button className="submit-btn cursor-pointer">
-              SHORTEN
-              <span>
-                <i className="fa-solid fa-wand-magic-sparkles"></i>
-              </span>
-            </button>
-          </div>
-          <div className="result-input">
-            <p className="result-link"> https://www.link_s/43s4534 </p>
-            <div className="copy-icon">
-              <i className="fa-regular fa-copy"></i>
+          <div className="form-section">
+            <div className="input-field">
+              <input
+                type="text"
+                className="search-inp"
+                placeholder=" https://www.link_shortner.com"
+              />
+              <button className="submit-btn cursor-pointer">
+                SHORTEN
+                <span>
+                  <i className="fa-solid fa-wand-magic-sparkles"></i>
+                </span>
+              </button>
+            </div>
+            <div className="result-input">
+              <p className="result-link"> https://www.link_s/43s4534 </p>
+              <div className="copy-icon">
+                <i className="fa-regular fa-copy"></i>
+              </div>
             </div>
           </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Navbar from './component/Navbar/Navbar.jsx'
-import Footer from './component/Footer/Footer.jsx'
-import MainSection from './component/MainSection.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Navbar from "./component/Navbar/Navbar.jsx";
+import Footer from "./component/Footer/Footer.jsx";
+import MainSection from "./component/MainSection/MainSection.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Navbar/>
-    <MainSection/>
-    <Footer/>
-  </React.StrictMode>,
-)
+    <Navbar />
+    <MainSection />
+    <Footer />
+  </React.StrictMode>
+);
